c7ec46371c7b0bf6efd68245bc60bffb238251f6
Malvin Lo <issv297@nus.edu.sg>
2018-06-22 16:11:14 +0800

    add ledger display

--- tradingInterface/trade/views.py
+++ tradingInterface/trade/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.urls import reverse
+import requests
 
-# Create your views here.
+# api addresses
+nodeAddress = "http://localhost"
+port = ":" + "10009" + "/" #handles user specifying invalid local host port
+apiVault = "api/example/ious"
+apiCreateIOU = "api/example/create-iou?iouValue="
+
+#helper functions
+def peerParse(astr):
+	party1 = astr[2:astr.find(",")]
+	location1 = astr[astr.find(",") + 4 : astr.find(",", astr.find(",") + 1)]
+	country1 = astr[astr.find("C=") + 2:]
+	peer = party1 + ", " + location1 + ", " + country1
+	return(peer)
+
+def listLogs(alist, ajson):  #parses ajson into alist of formatted strings
+	for i in ajson:
+		value = i['state']['data']['value']
+		borrower = str(peerParse(i['state']['data']['borrower']))
+		alist.append(borrower + " owing SGD" + str(value))
+
+# views
+def index(request):
+	return(render(request, 'trade/index.html'))
+
+def detail(request, hostport):
+	port = ":" + hostport + "/"
+	r = requests.get(nodeAddress + port + apiVault).json()
+	x = []
+	listLogs(x, r)
+	return render(request, 'trade/detail.html', {'rends':x})
+
