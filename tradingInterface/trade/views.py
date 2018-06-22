f0ffa6ea91d21e4ed36014fd09624133a10abee0
Malvin Lo <issv297@nus.edu.sg>
2018-06-22 16:27:14 +0800

    add owing totals

--- tradingInterface/trade/views.py
+++ tradingInterface/trade/views.py
@@ -22,6 +22,16 @@ def listLogs(alist, ajson):  #parses ajson into alist of formatted strings
 		borrower = str(peerParse(i['state']['data']['borrower']))
 		alist.append(borrower + " owing SGD" + str(value))
 
+def total(x):
+	totalB = 0
+	totalC = 0
+	for i in x:
+		if i.startswith("PartyB"):
+			totalB = totalB + int(i.split(" ")[5][3:])
+		elif i.startswith("PartyC"):
+			totalC = totalC + int(i.split(" ")[4][3:])
+	return(totalB, totalC)
+
 # views
 def index(request):
 	return(render(request, 'trade/index.html'))
@@ -31,5 +41,7 @@ def detail(request, hostport):
 	r = requests.get(nodeAddress + port + apiVault).json()
 	x = []
 	listLogs(x, r)
-	return render(request, 'trade/detail.html', {'rends':x})
+	totalB, totalC = total(x)
+	return render(request, 'trade/detail.html', 
+		{'rends':x, 'totalB':totalB, 'totalC':totalC})
 
